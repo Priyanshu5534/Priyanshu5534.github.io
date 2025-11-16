cbd7b9d582a1d3cf9754ae234f1427382df382d4
Priyanshu5534 <137056702+Priyanshu5534@users.noreply.github.com>
2025-11-16 14:02:14 +0530

    Delete comicverse/comicverse directory

--- comicverse/comicverse/index.tsx
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left blank to prevent the React application
-// from initializing. The project is now a pure HTML, CSS, and
-// JavaScript application driven by the scripts in the /js/ directory.
